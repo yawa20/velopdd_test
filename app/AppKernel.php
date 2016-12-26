86bd556ed6e3c28fcd3d982a2a16372823a6166a
Andrii Yakovlev <yawa20@gmail.com>
2016-12-26 15:20:52 +0200

    test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,6 +3,9 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+/**
+ *
+ */
 class AppKernel extends Kernel
 {
     public function registerBundles()
